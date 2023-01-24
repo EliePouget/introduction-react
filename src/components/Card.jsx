b5936ca1e1dbff6ba13a5e008ba9ea6f1b63b60a
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2023-01-24 09:03:48 +0100

    Modifiaction du composant Card pour extraire et décomposer les props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
-function Card(props) {
-
+function Card({ title='Title', ...props }) {
+    const { value, onChange } = props;
   return (
     <article className="card">
-      <header className="card__header header__title">{props.title}</header>
+      <header className="card__header header__title">{title}</header>
       <section className="card__main">Content</section>
     </article>    
   );
 }
-Card.defaultProps = {
-    title: 'Title'
-};
 
 export default Card;
